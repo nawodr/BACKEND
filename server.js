a032f88aa18da5dc7dec236cdc6c01ce1e676489
Nawod <nawodrandima64@gmail.com>
2021-06-30 15:30:39 +0530

    configered db & connect

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", ()=> {
+    console.log("mongodb connection success!");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is up and running on port number: ${PORT}`);
+})
